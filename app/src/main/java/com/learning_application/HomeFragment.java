ab104a771c43b68a846c7b1d8134fc8e94713122
Aarsh3nic <Patel333@ppmp>
2020-05-06 03:11:32 -0400

    Admin app database design done

--- app/src/main/java/com/learning_application/HomeFragment.java
+++ app/src/main/java/com/learning_application/HomeFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,20 +22,30 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.WriteBatch;
 import com.learning_application.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomeFragment extends Fragment implements AdapterView.OnItemSelectedListener {
     NavController navController;
     //CardView subjectCad;
-    TextView username,youtubeLink,pdfLink;
+    TextView username;
+    EditText youtubeLink,pdfLink;
     Button btnUpdateYoutube,btnUpdatePdf;
     FirebaseFirestore fStore;
     String standard, subject, chapter;
@@ -49,7 +60,7 @@ public class HomeFragment extends Fragment implements AdapterView.OnItemSelected
             chapter_11th_Physics_arraylist,chapter_11th_Chemistry_arraylist,chapter_11th_Maths_arraylist,
             chapter_12th_Physics_arraylist,chapter_12th_Chemistry_arraylist,chapter_12th_Maths_arraylist;
     ArrayAdapter<String> standard_adapter,subject_adapter,chapter_adapter;
-    
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -64,11 +75,11 @@ public class HomeFragment extends Fragment implements AdapterView.OnItemSelected
         subjectdrop = (Spinner) root.findViewById(R.id.subjectdrop);
         standarddrop = (Spinner) root.findViewById(R.id.standarddrop);
         chapterdrop = (Spinner) root.findViewById(R.id.chapterdrop);
-        youtubeLink = (TextView) root.findViewById(R.id.youtubeLink);
-        pdfLink = (TextView) root.findViewById(R.id.pdfLink);
+        youtubeLink = (EditText) root.findViewById(R.id.youtubeLink);
+        pdfLink = (EditText) root.findViewById(R.id.pdfLink);
         btnUpdatePdf = (Button) root.findViewById(R.id.btnUpdatePdf);
         btnUpdateYoutube = (Button) root.findViewById(R.id.btnUpdateYoutube);
-        
+
 
         standard_arraylist= new ArrayList<>();
         standard_arraylist.add("9th");
@@ -79,7 +90,7 @@ public class HomeFragment extends Fragment implements AdapterView.OnItemSelected
 
         standard_adapter  = new ArrayAdapter<>(getActivity(),android.R.layout.simple_spinner_item,standard_arraylist);
         standarddrop.setAdapter(standard_adapter);
-//
+        //
         //===================Subject Spinners coding starts======
 
         subject_9th_arraylist = new ArrayList<>();
@@ -168,79 +179,80 @@ public class HomeFragment extends Fragment implements AdapterView.OnItemSelected
                 switch (position){
                     case 0 : //------case 0 starts------------------------
                         subject_adapter  = new ArrayAdapter<>(getActivity(),
-                            android.R.layout.simple_spinner_item,subject_9th_arraylist);
-                    subjectdrop.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                        @Override
-                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                android.R.layout.simple_spinner_item,subject_9th_arraylist);
+                        subjectdrop.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                            @Override
+                            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-                            subject = parent.getItemAtPosition(position).toString();//Fetching Subject
+                                subject = parent.getItemAtPosition(position).toString();//Fetching Subject
 
-                            switch (position){
-                                case 0:chapter_adapter  = new ArrayAdapter<>(getActivity(),
-                                        android.R.layout.simple_spinner_item,chapter_9th_Maths_arraylist);
-                                ///Fetching Chapter process starts
-                                chapterdrop.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                                    @Override
-                                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                                        chapter = parent.getItemAtPosition(position).toString();//Fetching Chapter
-                                    }
+                                switch (position){
+                                    case 0:chapter_adapter  = new ArrayAdapter<>(getActivity(),
+                                            android.R.layout.simple_spinner_item,chapter_9th_Maths_arraylist);
+                                        ///Fetching Chapter process starts
+                                        chapterdrop.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                                            @Override
+                                            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                                chapter = parent.getItemAtPosition(position).toString();//Fetching Chapter
+                                            }
 
-                                    @Override
-                                    public void onNothingSelected(AdapterView<?> parent) {
+                                            @Override
+                                            public void onNothingSelected(AdapterView<?> parent) {
 
-                                    }
-                                });
-                                    ///Fetching Chapter process ends
-                                break;
-                                case 1:chapter_adapter  = new ArrayAdapter<>(getActivity(),
-                                        android.R.layout.simple_spinner_item,chapter_9th_Science_arraylist);
+                                            }
+                                        });
+                                        ///Fetching Chapter process ends
+                                        break;
+                                    case 1:chapter_adapter  = new ArrayAdapter<>(getActivity(),
+                                            android.R.layout.simple_spinner_item,chapter_9th_Science_arraylist);
 
-                                    ///Fetching Chapter process starts
-                                    chapterdrop.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                                        @Override
-                                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                                            chapter = parent.getItemAtPosition(position).toString();//Fetching Chapter
-                                        }
+                                        ///Fetching Chapter process starts
+                                        chapterdrop.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                                            @Override
+                                            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                                chapter = parent.getItemAtPosition(position).toString();//Fetching Chapter
+                                            }
 
-                                        @Override
-                                        public void onNothingSelected(AdapterView<?> parent) {
+                                            @Override
+                                            public void onNothingSelected(AdapterView<?> parent) {
 
-                                        }
-                                    });
-                                    ///Fetching Chapter process ends
-
-
-                                    break;
-                                case 2:chapter_adapter  = new ArrayAdapter<>(getActivity(),
-                                        android.R.layout.simple_spinner_item,chapter_9th_SocialScience_arraylist);
-                                    ///Fetching Chapter process starts
-                                    chapterdrop.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                                        @Override
-                                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                                            chapter = parent.getItemAtPosition(position).toString();//Fetching Chapter
-                                        }
-
-                                        @Override
-                                        public void onNothingSelected(AdapterView<?> parent) {
-
-                                        }
-                                    });
-                                    ///Fetching Chapter process ends
+                                            }
+                                        });
+                                        ///Fetching Chapter process ends
 
 
-                                    break;
-                                default: Log.d("tag", "Please choose first"); 
+                                        break;
+                                    case 2:chapter_adapter  = new ArrayAdapter<>(getActivity(),
+                                            android.R.layout.simple_spinner_item,chapter_9th_SocialScience_arraylist);
+                                        ///Fetching Chapter process starts
+                                        chapterdrop.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                                            @Override
+                                            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                                chapter = parent.getItemAtPosition(position).toString();//Fetching Chapter
+                                            }
+
+                                            @Override
+                                            public void onNothingSelected(AdapterView<?> parent) {
+
+                                            }
+                                        });
+                                        ///Fetching Chapter process ends
+
+
+                                        break;
+                                    default: Log.d("tag", "Please choose first");
+                                }
+                                chapterdrop.setAdapter(chapter_adapter);
                             }
-                            chapterdrop.setAdapter(chapter_adapter);
-                        }
 
-                        @Override
-                        public void onNothingSelected(AdapterView<?> parent) {
+                            @Override
+                            public void onNothingSelected(AdapterView<?> parent) {
 
-                        }
-                    });
-break;
-                        //------case 1 ends------------------------
+                            }
+                        });
+
+                        break;
+                    //------case 1 ends------------------------
 
 
                     case 1 : //------case 0 starts------------------------
@@ -318,7 +330,7 @@ break;
                             }
                         });
                         break;
-                        //------case 1 ends------------------------
+                    //------case 1 ends------------------------
 
 
                     case 2 : //------case 2 starts------------------------
@@ -396,7 +408,7 @@ break;
                             }
                         });
                         break;
-                        //------case 2 ends------------------------
+                    //------case 2 ends------------------------
 
 
                     case 3 : //------case 3 starts------------------------
@@ -474,9 +486,9 @@ break;
                             }
                         });
                         break;
-                        //------case 3 ends------------------------
-                        
-                        
+                    //------case 3 ends------------------------
+
+
                     default: Log.d("tag", "Please choose first");
                 }//switch ends
                 subjectdrop.setAdapter(subject_adapter);
@@ -510,27 +522,81 @@ break;
 
 
         });
-//////////////////////---------------------
+        //////////////////////---------------------
 
         ///////--------Button update PDF Link Starts-----------
 
-btnUpdatePdf.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
+        btnUpdatePdf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
 
-        Log.d("qwerty","Hi there :"+standard);
-        Log.d("qwerty","Hi there :"+subject);
-        Log.d("qwerty","Hi there :"+chapter);
+                ///-------------------Starting uploading links------------
+
+                DocumentReference documentReference = fStore.collection(standard).document(subject);
+
+                Map<String,Object> user = new HashMap<>();
+
+                documentReference.set(user)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d("qwerty", "Submitted Data "+ userId);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("qwerty", "onFailure: " + e.toString());
+                    }
+                });
+                documentReference.update(chapter+" PDF Links", FieldValue.arrayUnion(pdfLink.getText().toString()));
+
+                ///-------------------Ends uploading links------------
 
 
 
-
-    }//onclick method for pdf button ends here
-});
+            }//onclick method for pdf button ends here
+        });
 
 
         ///////--------Button update PDF Link ends-----------
+
+
+
+
+        ///////--------Button update You-Tube Link Starts-----------
+
+        btnUpdateYoutube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ///-------------------Starting uploading links------------
+
+                DocumentReference documentReference = fStore.collection(standard).document(subject);
+                Map<String,Object> user = new HashMap<>();
+                documentReference.set(user)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d("qwerty", "Submitted Data "+ userId);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("qwerty", "onFailure: " + e.toString());
+                    }
+                });
+                documentReference.update(chapter+" Youtube Links", FieldValue.arrayUnion(youtubeLink.getText().toString()));
+
+                ///-------------------Ends uploading links------------
+
+
+
+            }//onclick method for pdf button ends here
+        });
+
+
+        ///////--------Button update You-Tube Link ends-----------
         return root;
     }
 
@@ -545,30 +611,25 @@ btnUpdatePdf.setOnClickListener(new View.OnClickListener() {
     }
 
 
-//    public void updateYoutubeLink(View view){
-//
-//
-//
-//    }//updateYoutubeLink method ends
 
 
-//    @Override
-//    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-//        super.onActivityCreated(savedInstanceState);
-////        navController=Navigation.findNavController(this.getActivity(),R.id.nav_host_fragment);
-//        subjectCad.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //action_nav_home_to_subjects
-//                navController.navigate(R.id.action_nav_home_to_subjects);
-//            }
-//        });
-//    }
+    //    @Override
+    //    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+    //        super.onActivityCreated(savedInstanceState);
+    ////        navController=Navigation.findNavController(this.getActivity(),R.id.nav_host_fragment);
+    //        subjectCad.setOnClickListener(new View.OnClickListener() {
+    //            @Override
+    //            public void onClick(View v) {
+    //                //action_nav_home_to_subjects
+    //                navController.navigate(R.id.action_nav_home_to_subjects);
+    //            }
+    //        });
+    //    }
 
-//    @Override
-//    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-//        super.onViewCreated(view, savedInstanceState);
-//        navController=Navigation.findNavController(view);
-//
-//    }
+    //    @Override
+    //    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    //        super.onViewCreated(view, savedInstanceState);
+    //        navController=Navigation.findNavController(view);
+    //
+    //    }
 }//ends class
